83b2db0836749d17f8b9bdef4981b827053d9e4c
Prakhar Verma <unstoppableprakhar@gmail.com>
2025-03-04 11:40:45 -0500

    Add files via upload

---
+++ funny_echo/fucky_echo.ino
@@ -0,0 +1,23 @@
+int trig1=8;
+int echo1=9;
+long time;
+int dis;
+void setup() {
+ pinMode(trig1,OUTPUT);
+ pinMode(echo1,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  digitalWrite(trig1,HIGH);
+  digitalWrite(trig1,LOW);
+
+  time=pulseIn(echo1,HIGH);
+  dis=time*0.034/2;
+  Serial.print("distance=");
+  Serial.println(dis);
+ 
+  delay(500);
+
+}
